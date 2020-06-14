4e9364c836b19d268b2de876c3c17c1eb27b4772
obj153 <scintech.kgh@gmail.com>
2020-06-15 05:08:46 +0900

    Added main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import numpy as np
-from CoolProp.CoolProp as CP
+import CoolProp as CP
 
 def Turbine(Ti, Pi, Rp, fluid, eff):
     k = CP.PropsSI('C', 'P', Pi, 'T', Ti, fluid)
     To = Ti / (Rp ** ((k - 1) / k))
     return None
 
-print("Hello Git!")
-k = CP.PropsSI('Cp', 'P', 101325, 'T', 300, 'Air') / CP.PropsSI('Cv', 'P', 101325, 'T', 300, 'Air')
+
+Tst = CP.AbstractState("HEOS", "Air")
+PT = CP.CoolProp.generate_update_pair(CP.iP, 101325, CP.iT, 300)
+print(PT)
+Tst.update(PT[0],PT[1],PT[2])
+k = Tst.cvmass()
 print(k)
