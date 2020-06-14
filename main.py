62e2c860e82cf6571b6fe9c67ecdbb8bab14be48
obj153 <scintech.kgh@gmail.com>
2020-06-15 03:34:45 +0900

    Add Coolprop

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import numpy as np
+from CoolProp.CoolProp as CP
+
+def Turbine(Ti, Pi, Rp, fluid, eff):
+    k = CP.PropsSI('C', 'P', Pi, 'T', Ti, fluid)
+    To = Ti / (Rp ** ((k - 1) / k))
+    return None
 
 print("Hello Git!")
+k = CP.PropsSI('Cp', 'P', 101325, 'T', 300, 'Air') / CP.PropsSI('Cv', 'P', 101325, 'T', 300, 'Air')
+print(k)
