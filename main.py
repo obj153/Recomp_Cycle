9e3b315377c61f5de3d52abe13b5041081278887
obj153 <scintech.kgh@gmail.com>
2020-06-16 00:46:19 +0900

    ver 1.1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def Turbine(Ti, Pi, Rp, fluid, eff):
     Turb_in = CP.AbstractState("HEOS", fluid)
     PT = CP.CoolProp.generate_update_pair(CP.iP, Pi, CP.iT, Ti)
     Turb_in.update(PT[0],PT[1],PT[2])
-    k = Tst.cpmass() / Tst.cvmass()
+    k = Turb_in.cpmass() / Turb_in.cvmass()
     To_s = Ti / (Rp ** ((k - 1) / k))
     Po_s = Pi / Rp
     hi = CP.CoolProp.PropsSI('H', 'T', Ti, 'P', Pi, fluid)
@@ -19,7 +19,7 @@ def Compressor(Ti, Pi, Rp, fluid, eff):
     Comp_in = CP.AbstractState("HEOS", fluid)
     PT = CP.CoolProp.generate_update_pair(CP.iP, Pi, CP.iT, Ti)
     Comp_in.update(PT[0],PT[1],PT[2])
-    k = Tst.cpmass() / Tst.cvmass()
+    k = Comp_in.cpmass() / Comp_in.cvmass()
     To_s = Ti * (Rp ** ((k - 1) / k))
     Po_s = Pi * Rp
     hi = CP.CoolProp.PropsSI('H', 'T', Ti, 'P', Pi, fluid)
@@ -39,20 +39,43 @@ def Cooler(Ti, To, P, fluid, Rs):
     q_out = Rs * Cp * (Ti - To)
     return q_out
 
-def HTR(Th_i, Tl_i, eff):
+def HTR(Th_i, Tl_i, eff, Ph, Pl, fluid):
+    Ch = CP.CoolProp.PropsSI('C', 'T', Th_i, 'P', Pl, fluid)
+    Cl = CP.CoolProp.PropsSI('C', 'T', Tl_i, 'P', Ph, fluid)
     Th_o = Th_i - eff * (Th_i - Tl_i)
-    Tl_o = Tl_i + Th_o - Th_i
+    Tl_inter = Tl_i
+    for Th_inter in np.arange(Th_o, Th_i, 1):
+        Ch_inter = CP.CoolProp.PropsSI('C', 'T', Th_inter, 'P', Pl, fluid)
+        Cl_inter = CP.CoolProp.PropsSI('C', 'T', Tl_inter, 'P', Ph, fluid)
+        Tl_inter = Tl_inter + Ch * 1 / Cl
+        if Th_inter == Tl_inter:
+            break
+    Tl_o = Tl_inter
     return (Th_o, Tl_o)
 
 def LTR(Th_i, Tl_i, eff, Ph, Pl, fluid, Rs):
-    Ch = CP.CoolProp.PropsSI('C', 'T', Th_i, 'P', Ph, fluid)
-    Cl = CP.CoolProp.PropsSI('C', 'T', Tl_i, 'P', Pl, fluid) * Rs
+    Ch = CP.CoolProp.PropsSI('C', 'T', Th_i, 'P', Pl, fluid)
+    Cl = CP.CoolProp.PropsSI('C', 'T', Tl_i, 'P', Ph, fluid) * Rs
     if Ch < Cl:
         Th_o = Th_i - eff * (Th_i - Tl_i)
-        Tl_o = Tl_i + Ch * (Th_i - Th_o) / Cl
+        Tl_inter = Tl_i
+        for Th_inter in np.arange(Th_o, Th_i, 1):
+            Ch_inter = CP.CoolProp.PropsSI('C', 'T', Th_inter, 'P', Pl, fluid)
+            Cl_inter = CP.CoolProp.PropsSI('C', 'T', Tl_inter, 'P', Ph, fluid)
+            Tl_inter = Tl_inter + Ch * 1 / Cl
+            if Th_inter == Tl_inter:
+                break
+        Tl_o = Tl_inter
     else:
         Tl_o = Tl_i + eff * (Th_i - Tl_i)
-        Th_o = Th_i - Cl * (Tl_o - Tl_i) / Ch
+        Th_inter = Th_i
+        for Tl_inter in np.arange(Tl_o, Tl_i, -1):
+            Ch_inter = CP.CoolProp.PropsSI('C', 'T', Th_inter, 'P', Pl, fluid)
+            Cl_inter = CP.CoolProp.PropsSI('C', 'T', Tl_inter, 'P', Ph, fluid)
+            Th_inter = Th_inter + Cl * (-1) / Ch
+            if Th_inter == Tl_inter:
+                break
+        Th_o = Th_inter
     return (Th_o, Tl_o)
 
 def Mixing(T1, T2, P, Rs, fluid):
@@ -62,17 +85,6 @@ def Mixing(T1, T2, P, Rs, fluid):
     Tf = CP.CoolProp.PropsSI('T', 'H', Hf, 'P', P, fluid)
     return Tf
         
-
-Tst = CP.AbstractState("HEOS", "Air")
-PT = CP.CoolProp.generate_update_pair(CP.iP, 101325, CP.iT, 300)
-print(PT)
-Tst.update(PT[0],PT[1],PT[2])
-k = Tst.cpmass() / Tst.cvmass()
-print(k)
-
-
-print (Turbine(550 + 273.15, 25 * 10 ** 6, 9, "CO2", 0.8))
-print (Mixing(300, 400, 10131500, 0.5, "CO2"))
 
 
 # Recompression Loop
@@ -86,8 +98,8 @@ Heat_exchanger_eff = 0.95
 fluid = "CO2"
 Ans_list = []
 
-for Rp in range(0, 15, 0.2):
-    for Rs in range(0, 1, 0.05):
+for Rp in np.arange(1.1, 3, 0.1):
+    for Rs in np.arange(0.3, 0.7, 0.05):
         Turbine_outlet = Turbine(T5, P_High, Rp, fluid, Turbine_eff)
         P_Low = Turbine_outlet[1]
         T6 = Turbine_outlet[2]
@@ -103,16 +115,18 @@ for Rp in range(0, 15, 0.2):
             T3a = RC_outlet[2]
             T3 = Mixing(T3b, T3a, P_High, Rs, fluid)
             T7_old = T7
-            HTR_outlet = HTR(T6, T3, Heat_exchanger_eff)
+            HTR_outlet = HTR(T6, T3, Heat_exchanger_eff, P_High, P_Low, fluid)
             T7 = HTR_outlet[0]
             T4 = HTR_outlet[1]
         q_in = Heater(T4, T5, P_High, fluid)
-        q_out = Cooler(T8, T1, P_Low, fluid)
+        q_out = Cooler(T8, T1, P_Low, fluid, Rs)
         w_out = q_in - q_out
         Total_eff = w_out / q_in * 100
         Ans_list.append([Rp, Rs, Total_eff])
+        print([Rp,Rs])
 
-print Ans_list
+for i in Ans_list:
+    print (i)
 
 
 
